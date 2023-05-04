50dbf3e4cab8fff76bb86fc9dd03ce425923fcf7
Alden <115692203+AldenALT@users.noreply.github.com>
2023-05-04 19:04:25 +0700

    Update keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -1,13 +1,33 @@
-from flask import Flask
-from flask import Flask
+import os
+import json
+import logging
 from threading import Thread
+from datetime import datetime
+from flask import Flask, jsonify, request
+
+os.environ['FLASK_ENV'] = 'production'
 
 app = Flask('')
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.WARNING)
 
 
 @app.route('/')
 def index():
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] ping from {request.remote_addr}")
     return '<pre>OK</pre>', 200
+
+
+@app.route('/tracker')
+def tracker():
+    try:
+        data = json.load(open('tracker.json'))
+        data['total'] = data['good'] + data['bad']
+        return jsonify(data), 200
+    except Exception as err:
+        print(err)
+        return f'<pre>{err}</pre>', 500
 
 
 def run():
